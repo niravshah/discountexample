6924b974d34573d54f9f0069be6c7c11b76a5a24
niravshah <nirav.shah83@gmail.com>
2016-04-26 12:35:47 +0100

    TDD - Added Test for Home Resource. Fails now. Will create the Home Resource next.

---
+++ src/test/java/shop/resources/HomeResourceTest.java
@@ -0,0 +1,55 @@
+package shop.resources;
+
+import com.jayway.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import shop.Application;
+
+import java.util.HashMap;
+
+import static com.jayway.restassured.RestAssured.when;
+import static com.jayway.restassured.http.ContentType.JSON;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * Created by Nirav on 26/04/2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class HomeResourceTest {
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void home() {
+        HashMap cartLink =
+                when().
+                        get("/api").
+                        then().
+                        contentType(JSON).
+                        statusCode(HttpStatus.SC_OK).
+                        body("message", Matchers.is("Home Message")).
+                        extract().
+                        path("_links");
+
+        assertThat(cartLink.size(), equalTo(4));
+
+    }
+}
