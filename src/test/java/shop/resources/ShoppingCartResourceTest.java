420925fea42a598eb175853edb89e5a2bb53efc3
niravshah <nirav.shah83@gmail.com>
2016-04-26 12:48:41 +0100

    TDD - New tests for Shopping Cart Resource. We will use this resource to get the current Shopping Cart and to add new Products to the shopping Cart !

---
+++ src/test/java/shop/resources/ShoppingCartResourceTest.java
@@ -0,0 +1,68 @@
+package shop.resources;
+
+import com.jayway.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import shop.Application;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
+import static com.jayway.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+
+/**
+ * Created by Nirav on 26/04/2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class ShoppingCartResourceTest {
+
+
+    @Value("${local.server.port}")
+    int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void get() {
+        when().
+                get("/api/cart").
+                then().
+                contentType(JSON).
+                statusCode(HttpStatus.SC_OK).
+                body("message", Matchers.is("")).
+                body("cart", empty()).
+                extract().
+                path("_links");
+    }
+
+    @Test
+    public void add() {
+        String product = "Widget";
+        given()
+                .param("product", product)
+                .param("qty", 10)
+                .post("/api/cart")
+                .then()
+                .contentType(JSON)
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", Matchers.is(String.format("")))
+                .body("cart", is(not(empty())))
+                .body("cart[0].product.productCode", Matchers.is(product));
+    }
+}
